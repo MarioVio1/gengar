ed569415d1510b193f027af663fd1afa0643d236
MarioVio1 <gattom22@gmail.com>
2026-03-27 14:55:07 +0100

    Configure remote image patterns in next.config.ts
    Added remote image patterns for external sources.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'iili.io',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
